6ef66349bc000b29e4f2cbbc75a27c72c0be5569
Erika Yao <erikayao@amazon.com>
2026-06-11 11:01:15 -0700

    update manifest and test files

--- tests/integration/build_invoke/node/test_node_20.py
+++ tests/integration/build_invoke/node/test_node_20.py
@@ -8,52 +8,64 @@ For each template, it will test the following sam commands:
 """
 
 class BuildInvoke_nodejs20_x_cookiecutter_aws_sam_hello_nodejs(BuildInvokeBase.SimpleHelloWorldBuildInvokeBase):
-    directory = "nodejs20.x/hello"
+    runtime = "nodejs20.x"
+    directory = "nodejs/hello"
 
 class BuildInvoke_nodejs20_x_cookiecutter_aws_sam_step_functions_sample_app(BuildInvokeBase.BuildInvokeBase):
-    directory = "nodejs20.x/step-func"
+    runtime = "nodejs20.x"
+    directory = "nodejs/step-func"
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_from_scratch(BuildInvokeBase.BuildInvokeBase):
-    directory = "nodejs20.x/scratch"
+    runtime = "nodejs20.x"
+    directory = "nodejs/scratch"
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_cloudwatch_events(BuildInvokeBase.BuildInvokeBase):
-    directory = "nodejs20.x/cw-event"
+    runtime = "nodejs20.x"
+    directory = "nodejs/cw-event"
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_s3(BuildInvokeBase.BuildInvokeBase):
-    directory = "nodejs20.x/s3"
+    runtime = "nodejs20.x"
+    directory = "nodejs/s3"
 
     # if we want to check response json, we need to setup bucket for testing
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_sns(BuildInvokeBase.BuildInvokeBase):
-    directory = "nodejs20.x/sns"
+    runtime = "nodejs20.x"
+    directory = "nodejs/sns"
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_sqs(BuildInvokeBase.BuildInvokeBase):
-    directory = "nodejs20.x/sqs"
+    runtime = "nodejs20.x"
+    directory = "nodejs/sqs"
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_response_streaming(BuildInvokeBase.BuildInvokeBase):
-    directory = "nodejs20.x/response-streaming"
+    runtime = "nodejs20.x"
+    directory = "nodejs/response-streaming"
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_web(BuildInvokeBase.QuickStartWebBuildInvokeBase):
-    directory = "nodejs20.x/web"
+    runtime = "nodejs20.x"
+    directory = "nodejs/web"
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_quick_start_full_stack(BuildInvokeBase.QuickStartWebBuildInvokeBase):
-    directory = "nodejs20.x/full-stack"
+    runtime = "nodejs20.x"
+    directory = "nodejs/full-stack"
 
 
 class BuildInvoke_image_nodejs20_x_cookiecutter_aws_sam_hello_nodejs_lambda_image(
     BuildInvokeBase.SimpleHelloWorldBuildInvokeBase
 ):
-    directory = "nodejs20.x/hello-img"
+    runtime = "nodejs20.x"
+    directory = "nodejs/hello-img"
 
 
 class BuildInvoke_nodejs20_x_cookiecutter_aws_sam_gql_quick_start(BuildInvokeBase.BuildInvokeBase):
-    directory = "nodejs20.x/hello-gql"
+    runtime = "nodejs20.x"
+    directory = "nodejs/hello-gql"
     should_test_lint = False
